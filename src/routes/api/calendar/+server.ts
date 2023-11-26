064c8df8a9d946dfa075f7b91734c53927e0c795
Sven Haiges <sven.haiges@sap.com>
2023-11-26 12:07:21 +0100

    timezone...

--- src/routes/api/calendar/+server.ts
+++ src/routes/api/calendar/+server.ts
@@ -9,15 +9,15 @@ export async function GET() {
     const raw = await fetch('https://calendar.google.com/calendar/ical/2274266366b1c3f4a2baf9b3a3fc4ca3f2224909ca49e61456a18a4043cafd76%40group.calendar.google.com/public/basic.ics').then(res => res.text())
     const data = ical.parseICS(raw);
 
-    //console.log(data);    
+    console.log(data);    
     let events = [];
 
     for (let k in data) {
         if (data.hasOwnProperty(k)) {
             const ev = data[k];
             if (data[k].type == 'VEVENT') {
-                events[events.length] = {start:new Date(ev.start) ,summary:ev.summary, location:ev.location, date:`${ev.start.getDate()} of ${months[ev.start.getMonth()]}`, time:`${ev.start.toLocaleTimeString('en-GB', { hour: "2-digit", minute: "2-digit" })}`}
-                //`${ev.summary} is in ${ev.location} on the ${ev.start.getDate()} of ${months[ev.start.getMonth()]} at ${ev.start.toLocaleTimeString('en-GB', { hour: "2-digit", minute: "2-digit" })}`);
+                let offset = 1000 * 60 * 60;
+                events[events.length] = {start:new Date(ev.start) ,summary:ev.summary, location:ev.location, date:`${ev.start.getDate()} of ${months[ev.start.getMonth()]}`, time:`${ev.start.toLocaleTimeString('de-DE', { hour: "2-digit", minute: "2-digit" })}`}
             }
         }
     }
